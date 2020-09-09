8a70a589e8bab5ca6a92a39e8eacea83fcfa4562
unknown <michael@site469.com>
2020-09-09 16:37:16 -0400

    download working

--- download.py
+++ download.py
@@ -1,16 +1,15 @@
-import re
 from selenium import webdriver
 import browser_cookie3
-
-def expand_element(element):
-    shadow_root = driver.execute_script('return arguments[0].shadowRoot', element)
-    return shadow_root
+import time
+import requests
+import re
 
 driver = webdriver.Chrome('./chromedriver')
 
-# visit home page
 learn_url = "https://learn.uwaterloo.ca"
 bongo_url = "https://bongo-ca.youseeu.com/spa/student/class/190649/meeting-preview/479604?lti-scope=d2l-resource-syncmeeting-open"
+
+# visit home page
 driver.get(learn_url)
 
 # set cookies
@@ -27,24 +26,18 @@ for c in learn_cookies:
 # then visit the home page again, should sign in. Then we go to the lesson
 driver.get(learn_url)
 driver.get(bongo_url)
+time.sleep(10)
 
-# now we get the video from the lesson page and download it
-
-element_root = driver.find_elements_by_id("root")
-# element_root_shadow = expand_element(element_root)
-# settingsBasicPage = settingsShadowRoot.find_element_by_tag_name("settings-basic-page")
-# settingsBasicPageShadowroot = expand_root_element(settingsBasicPage)
-# settingsPrivacyPage = settingsBasicPageShadowroot.find_element_by_tag_name("settings-privacy-page")
-# settingsPrivacyShadowRoot = expand_root_element(settingsPrivacyPage)
-# settingsClearBrowsingDataDialog = settingsPrivacyShadowRoot.find_element_by_tag_name(
-#     "settings-clear-browsing-data-dialog")
-# settingsClearBrowsingDataDialogShadowRoot = expand_root_element(settingsClearBrowsingDataDialog)
-# settingsClearBrowsingDataDialogShadowRoot.find_element_by_id("clearBrowsingDataConfirm").click()
-
-ids = driver.find_elements_by_xpath('//*[@id]')
-for ii in ids:
-    # print(ii.tag_name)
-    print(ii.get_attribute('id'))    # id name as string
+# now we get the video from the lesson page and download it 
+video_element = driver.find_elements_by_id("vjs_video_3_html5_api")[0]
+video_url = video_element.get_attribute("src")
 
 driver.quit()
-# print(video_url)
+
+print('Got video URL. Beginning download...')
+r = requests.get(video_url)
+
+with open('C:\\Users\\Mike\\Documents\\4aSchool\\test.mp4', 'wb') as f:
+    f.write(r.content)
+
+print('done')
